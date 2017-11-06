9b56a46801c4f97ca2d36c049270f76c5da18e19
Allan Chang <allanc1147@gmail.com>
2017-11-06 12:30:39 -0500

    Image logs its own history. Image can be moved, renamed, have tags added, have tags removed.

--- ProjectPhase1/src/Image.java
+++ ProjectPhase1/src/Image.java
@@ -17,7 +17,20 @@ public class Image {
      */
     private String name;
 
-    // TODO : Add directory for an image (create Log object)
+    /**
+     * The history of the image
+     */
+    private Log log;
+
+    /**
+     * Extension of image
+     */
+    private String extension;
+
+    /**
+     * Directory of image
+     */
+    private File directory;
 
     /**
      * Construct an image representing File imageFile
@@ -27,9 +40,13 @@ public class Image {
         // Precondition: imageFile is valid
         this.imageFile = imageFile;
         tags = new ArrayList<>();
+        log = new Log();
+        directory = imageFile.getParentFile();
 
         String rawName = imageFile.getName();
-        // Todo Check how extension is displayed
+        int indexExtension = rawName.lastIndexOf(".");
+        extension = rawName.substring(indexExtension);
+        rawName = rawName.substring(0, indexExtension);
         String[] nameParts = rawName.split(" @");
 
         name = nameParts[0];
@@ -46,30 +63,6 @@ public class Image {
      */
     public boolean containsTag(String tag){
         return tags.contains(tag);
-    }
-
-    /**
-     * Renames this Image and the File this Image represents
-     * @param newName The Image's new name
-     */
-    public void rename(String newName){
-        this.name = newName;
-
-        StringBuilder fileName = new StringBuilder();
-        fileName.append(newName);
-        for(String tag:tags){
-            fileName.append(" @");
-            fileName.append(tag);
-        }
-        // Todo Check how extension is added
-        File newLocation = new File(imageFile.getParentFile(), fileName.toString());
-        // Todo Check what if name already exists? What if newLocation = imageFile?
-        boolean success = imageFile.renameTo(newLocation);
-
-        if(!success){
-            // Todo Set more specific exception or make exception
-            throw new RuntimeException();
-        }
     }
 
     /**
@@ -101,5 +94,99 @@ public class Image {
      */
     public File getFile(){
         return new File(imageFile.toString());
+    }
+
+    /**
+     * Renames this Image and the File this Image represents
+     * @param newName The Image's new name
+     */
+    public void rename(String newName){
+        String oldName = this.name;
+        this.name = newName;
+
+        updateFile("Image \"" + oldName + "\" was renamed to \"" + newName + "\"");
+    }
+
+    /**
+     * Moves this Image to newDirectory
+     * @param newDirectory the directory that will store the image
+     */
+    public void move(File newDirectory){
+        // Precondition: Assume directory is valid
+        directory = newDirectory;
+        updateFile("Moved image \"" + name + "\" to \"" + newDirectory.toString() + "\"");
+    }
+
+    /**
+     * Adds tag to image
+     * @param tag to add
+     */
+    public void addTag(String tag){
+        if(!tags.contains(tag)){
+            tags.add(tag);
+            updateFile("Added tag \"" + tag + "\" to image \"" + name + "\"");
+        }
+    }
+
+    /**
+     * Removes tag from image
+     * @param tag tag to remove
+     */
+    public void removeTag(String tag){
+        if(tag.contains(tag)){
+            tags.remove(tag);
+            updateFile("Removed tag \"" + tag + "\" from image \"" + name + "\"");
+        }
+    }
+
+    /**
+     * Update image file using current image properties and record log with logMessage
+     */
+    private void updateFile(String logMessage){
+        boolean success = imageFile.renameTo(createLocation());
+        if(success){
+            log.addEntry(new Entry(logMessage));
+        }
+    }
+
+    /**
+     * Create image file using current image properties
+     * @return File representation of this image
+     */
+    private File createLocation(){
+        StringBuilder fileName = new StringBuilder();
+        fileName.append(name);
+        for(String tag:tags){
+            fileName.append(" @");
+            fileName.append(tag);
+        }
+        fileName.append(extension);
+        File location = new File(directory, fileName.toString());
+        // Todo Check what if name already exists? What if newLocation = imageFile?
+        System.out.println(location.toString());
+        return location;
+    }
+
+    public static void main(String[] args) {
+        //TODO used to debug Image. Delete in final submission
+        File imageFile = new File("C:\\Users\\allan\\Downloads\\csc207 test\\Tester.png");
+
+        Image image = new Image(imageFile);
+        image.rename("Test");
+        image.rename("Tester");
+        /**
+        image.addTag("test");
+        image.addTag("budapest");
+        image.addTag("budapest");
+        **/
+
+        System.out.println(image.getName());
+        System.out.println(image.getTags().toString());
+        System.out.println(image.extension);
+
+        for(Entry e : image.log){
+            System.out.println(e.getEntryName());
+        }
+
     }
 }
