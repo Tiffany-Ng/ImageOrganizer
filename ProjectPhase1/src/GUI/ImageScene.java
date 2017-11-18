2aa969f8ee8e09af1ba28cf6717bdb2454ffe802
Akshat <akshat998@gmail.com>
2017-11-18 04:06:11 -0500

    Implemented extensive style changes in ImageScene: Made methods where appropriate, added comments & docStrings, made code more redabe

--- ProjectPhase1/src/GUI/ImageScene.java
+++ ProjectPhase1/src/GUI/ImageScene.java
@@ -22,20 +22,17 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.io.File;
+import java.util.logging.Level;
 
 /** GUI of an individual image's information */
-public class ImageScene {
-
-  /** Combo box to hold previously used tags */
-  ComboBox newTag;
+ class ImageScene {
 
   /** The image used . */
   private ImageFile image;
 
-  /** The actual scene with hold all the elements. */
+  /** The actual scene which hold all the elements. */
   private Scene imageScene;
 
   /** Main pane of the scene. */
@@ -57,78 +54,42 @@ public class ImageScene {
   private ComboBox<String> imageNames;
 
   /**
-   * Construct an GUI.ImageScene.
+   * Construct the GUI view of an image
    *
-   * @param image
+   * @param image particular image in a directory
+   * @param directory Location of the file containing images
+   * @param prevScene Reference to GUI with all images in a directory
+   * @throws IOException Case when invalid directory
    */
-  public ImageScene(ImageFile image, File directory, Stage prevScene) throws IOException {
+   ImageScene(ImageFile image, File directory, Stage prevScene) throws IOException {
 
     this.image = image;
 
     // inspired from https://docs.oracle.com/javafx/2/layout/builtin_layouts.htm
-    g = gridSetup();
+    this.g = gridSetup();
     imageScene = new Scene(g);
 
     this.directory = directory;
     this.prevScene = prevScene;
 
     prevScene.setMaximized(false);
-    // prevScene.sizeToScene();
     prevScene.setWidth(1325);
     prevScene.setHeight(750);
 
     // Source: https://stackoverflow.com/questions/13702191/center-location-of-stage (Nov 16, 2017)
     prevScene.setX(200);
     prevScene.setY(100);
-
     prevScene.setResizable(false);
   }
 
-  /**
-   * Setup of the whole screen
+
+  /** cite : https://stackoverflow.com/questions/13880638/how-do-i-pick-up-the-enter-key-being-pressed-in-javafx2
+   * rename the image to the name collected from the relevant texBox(on hitting enter)
    *
-   * @return GridPane
+   * @param name the textBox which contains the new name for the image
    */
-  private GridPane gridSetup() throws IOException {
-
-    GridPane layout = new GridPane();
-    layout.setHgap(6);
-    layout.setVgap(6);
-    layout.setPadding(new Insets(10, 10, 10, 10));
-
-    // image in form of a viewable icon
-    ImageView icon = new ImageView();
-
-    // https://stackoverflow.com/questions/27894945/how-do-i-resize-an-imageview-image-in-javafx
-    icon.setFitWidth(720);
-    icon.setFitHeight(480);
-    icon.setPreserveRatio(true);
-
-    // needs the "file://" because image will not understand it is a directory
-    // https://stackoverflow.com/questions/8474694/java-url-unknown-protocol-c
-    icon.setImage(new Image("file:///" + image.getFile().toString()));
-
-    // flowPane for image information
-    VBox f = vBoxSetup();
-
-    //    HBox h = new HBox();
-    //    h.getChildren().add(icon);
-    //    h.getChildren().add(f);
-    //    h.setSpacing(5);
-
-    layout.add(icon, 1, 2, 4, 2);
-    layout.add(f, 6, 2, 2, 2);
-
-    // go to main screen
-    Button back = new Button();
-    back.setText("<- Back");
-    layout.add(back, 0, 0, 1, 1);
-
-    TextField name = new TextField(image.getName());
-    name.setEditable(true);
-
-    // https://stackoverflow.com/questions/13880638/how-do-i-pick-up-the-enter-key-being-pressed-in-javafx2
-    name.setOnKeyPressed(
+  private void renameImageFile(TextField name){
+        name.setOnKeyPressed(
         k -> {
           if (k.getCode().equals(KeyCode.ENTER)) {
 
@@ -143,25 +104,22 @@ public class ImageScene {
 
             } else {
 
+              addClickableTags();
               updateLog();
               imageNameUpdate();
             }
           }
         });
+  }
 
-    back.setOnAction(
-        e -> {
-          new PicGrid(prevScene, this.directory).picGrid();
-        });
-
-    // https://docs.oracle.com/javafx/2/ui_controls/combo-box.htm
-    imageNames = new ComboBox<>();
-    imageNameUpdate();
-    imageNames.setMaxWidth(590);
-    imageNames.getSelectionModel().selectFirst();
-
-    Button revertName = new Button("Revert");
-    revertName.setOnAction(
+  /**
+   * Adding the ability to revert to an older name on the GUI
+   *
+   * @param revertName Button that initiates action
+   * @param name The new (changed)name to be displayed the textField
+   */
+  private void revertOldTagName(Button revertName, TextField name){
+        revertName.setOnAction(
         event -> {
           if (!imageNames.getSelectionModel().isEmpty()
               && !image.nameWithTags().equals(imageNames.getValue())) {
@@ -178,17 +136,18 @@ public class ImageScene {
             name.setText(image.getName());
           }
         });
-
-    HBox imageName = new HBox();
-    imageName.getChildren().addAll(name, imageNames, revertName);
-    imageName.setSpacing(5.0);
-
-    layout.add(imageName, 1, 0, 1, 1);
-
-    layout.setPrefHeight(700);
-    layout.setPrefWidth(1325);
-    return layout;
   }
+
+    /**
+     * Check if the string entered by the user on the GUI is a valid tag
+     *
+     * @param newTag field which contains the new tag name
+     * @return true if valid tag name.
+     */
+  private boolean checkValidTagName(ComboBox newTag){
+      return (newTag.getValue() instanceof String) && ((String) newTag.getValue()).length() != 0;
+  }
+
 
   /** Update the comboBox of image names. */
   private void imageNameUpdate() {
@@ -196,7 +155,6 @@ public class ImageScene {
     imageNames.getItems().removeAll(imageNames.getItems());
 
     for (String name : image.getPriorNames()) {
-
       imageNames.getItems().add(name);
     }
 
@@ -205,6 +163,124 @@ public class ImageScene {
       imageNames.setPromptText(imageNames.getItems().get(0));
     }
   }
+
+  /** Update the log with its new information. */
+  private void updateLog() {
+
+    Log imageLog = image.getLog();
+    StringBuilder logs = new StringBuilder();
+
+    // add all logs as a line
+    for (Entry e : imageLog) {
+
+      // https://stackoverflow.com/questions/15977295/control-for-displaying-multiline-text
+      logs.append(e.toString()).append(System.lineSeparator());
+    }
+
+    // represent log as textArea
+    log.setText(logs.toString());
+  }
+
+  /**
+   * * Return the scene which holds the elements
+   * @return current interaction's screen
+   */
+   Scene getImageScene() {
+    return imageScene;
+   }
+
+    /**
+     * Add all tags image has into a clickable section of the scene.
+     *
+     * @return FlowPane
+     */
+    private FlowPane addClickableTags() {
+
+        // https://stackoverflow.com/questions/37378973/implement-tags-bar-in-javafx
+        List<String> tags = image.getTags();
+        f.getChildren().removeAll(f.getChildren());
+
+        ImageView i = new ImageView(new Image("file:///" + "x.jpeg"));
+
+        for (String t : tags) {
+
+            // makes all tags as clickable buttons
+            Button tag = new Button(t, i);
+
+            tag.setOnAction(
+                    e -> {
+                        image.removeTag(tag.getText());
+                        f.getChildren().remove(tag);
+                        updateLog();
+                        imageNameUpdate();
+                    });
+
+            f.getChildren().add(tag);
+        }
+        return f;
+    }
+
+
+  /**
+   * Setup of the whole screen
+   *
+   * @return GridPane
+   */
+  private GridPane gridSetup() throws IOException {
+
+    GridPane layout = new GridPane();
+    layout.setHgap(6);
+    layout.setVgap(6);
+    layout.setPadding(new Insets(10, 10, 10, 10));
+
+    // image in form of a viewable icon
+    ImageView icon = new ImageView();
+    // https://stackoverflow.com/questions/27894945/how-do-i-resize-an-imageview-image-in-javafx
+    icon.setFitWidth(720);
+    icon.setFitHeight(480);
+    icon.setPreserveRatio(true);
+
+    // needs the "file://" because image will not understand it is a directory
+    // https://stackoverflow.com/questions/8474694/java-url-unknown-protocol-c
+    icon.setImage(new Image("file:///" + image.getFile().toString()));
+    layout.add(icon, 1, 2, 4, 2);
+
+    // flowPane for image information
+    VBox f = vBoxSetup();
+    layout.add(f, 6, 2, 2, 2);
+
+    // go to screen with all images
+    Button back = new Button();
+    back.setText("<- Back");
+    layout.add(back, 0, 0, 1, 1);
+
+    TextField name = new TextField(image.getName());
+    name.setEditable(true);
+    this.renameImageFile(name);
+
+    back.setOnAction(
+        e -> new PicGrid(prevScene, this.directory).picGrid());
+
+    // https://docs.oracle.com/javafx/2/ui_controls/combo-box.htm
+    imageNames = new ComboBox<>();
+    imageNameUpdate();
+    imageNames.setMaxWidth(590);
+    imageNames.getSelectionModel().selectFirst();
+
+    Button revertName = new Button("Revert");
+    this.revertOldTagName(revertName, name);  // gives functionality to the Button
+
+    HBox imageName = new HBox();
+    imageName.getChildren().addAll(name, imageNames, revertName);
+    imageName.setSpacing(5.0);
+    layout.add(imageName, 1, 0, 1, 1);
+
+    layout.setPrefHeight(700);
+    layout.setPrefWidth(1325);
+    return layout;
+  }
+
+
 
   /**
    * Setup the image's information in a flowPane.
@@ -225,13 +301,12 @@ public class ImageScene {
     // button for opening the directory
     Button openDir = new Button();
     openDir.setText("Open Directory");
-
     openDir.setOnAction(
         e -> {
           try {
             Desktop.getDesktop().open(image.getDirectory());
           } catch (IOException ex) {
-            Main.logger.warning("Can't open directory");
+            Main.logger.log(Level.SEVERE, "Can't open directory", ex);
           }
         });
 
@@ -240,9 +315,7 @@ public class ImageScene {
     changeDir.setText("Change Directory");
 
     changeDir.setOnAction(
-        e -> {
-          DirChooser.dirChooser(prevScene, this.image, directory);
-        });
+        e -> DirChooser.dirChooser(prevScene, this.image, directory));
 
     HBox dir = new HBox();
     dir.boundsInParentProperty();
@@ -273,6 +346,7 @@ public class ImageScene {
               newTag.setVisibleRowCount(10);
             });
 
+    // TODO: seems redundant...remove?
     //            newTag.getEditor().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
     //              if (event.getCode() == KeyCode.ENTER) {
     //                  System.out.println("kdjla");
@@ -288,36 +362,36 @@ public class ImageScene {
     //              }
     //            });
 
-    newTag
-        .getEditor()
-        .textProperty()
-        .addListener(
-            (observable, oldValue, newValue) -> {
-              LinkedList<String> relatedTags = new LinkedList<>();
-
-              /*Platform.runLater(
-                      () -> {
-                          newTag.getItems().clear();
-                          if (newValue.length() == 0) {
-                              newTag.getItems().addAll(TagManager.tags);
-                              newTag.setVisibleRowCount(10);
-                          } else {
-                              relatedTags.addAll(TagManager.search(newValue));
-                              newTag.setVisibleRowCount(relatedTags.size());
-                              newTag.getItems().addAll(relatedTags);
-                          }
-
-                      });
-              newTag.getEditor().setText(newValue);
-              */
-
-            });
+//    newTag
+//        .getEditor()
+//        .textProperty()
+//        .addListener(
+//            (observable, oldValue, newValue) -> {
+//              LinkedList<String> relatedTags = new LinkedList<>();
+//
+//              /*Platform.runLater(
+//                      () -> {
+//                          newTag.getItems().clear();
+//                          if (newValue.length() == 0) {
+//                              newTag.getItems().addAll(TagManager.tags);
+//                              newTag.setVisibleRowCount(10);
+//                          } else {
+//                              relatedTags.addAll(TagManager.search(newValue));
+//                              newTag.setVisibleRowCount(relatedTags.size());
+//                              newTag.getItems().addAll(relatedTags);
+//                          }
+//
+//                      });
+//              newTag.getEditor().setText(newValue);
+//              */
+//
+//            });
 
     HBox tagBox = new HBox();
     Button addTag = new Button("+");
     addTag.setOnAction(
         e -> {
-          if (newTag.getValue() instanceof String && ((String) newTag.getValue()).length() != 0) {
+          if (checkValidTagName(newTag)) {   // TODO: change this if-clause
             image.addTag((String) newTag.getValue());
             newTag.setValue("");
           }
@@ -330,7 +404,7 @@ public class ImageScene {
     Button addToAll = new Button("Add to All");
     addToAll.setOnAction(
         e -> {
-          if (newTag.getValue() instanceof String && ((String) newTag.getValue()).length() != 0) {
+          if (checkValidTagName(newTag)) {
             ImageManager.addGlobalTag((String) newTag.getValue());
             newTag.setValue("");
           }
@@ -340,7 +414,7 @@ public class ImageScene {
     Button deleteFromAll = new Button("Delete from All");
     deleteFromAll.setOnAction(
         e -> {
-          if (newTag.getValue() instanceof String && ((String) newTag.getValue()).length() != 0) {
+          if (checkValidTagName(newTag)) {
             ImageManager.deleteGlobalTag((String) newTag.getValue());
             newTag.setValue("");
           }
@@ -374,63 +448,10 @@ public class ImageScene {
 
     // wrap error solution https://stackoverflow.com/questions/29537264/javafx-flowpane-autosize
     log.setPrefHeight(480 / 2);
-
     flow.getChildren().add(log);
 
     return flow;
   }
 
-  /**
-   * Add all tags image has into a clickable section of the scene.
-   *
-   * @return FlowPane
-   */
-  private FlowPane addClickableTags() {
 
-    // https://stackoverflow.com/questions/37378973/implement-tags-bar-in-javafx
-    List<String> tags = image.getTags();
-    f.getChildren().removeAll(f.getChildren());
-
-    ImageView i = new ImageView(new Image("file:///" + "x.jpeg"));
-
-    for (String t : tags) {
-
-      // makes all tags as clickable buttons
-      Button tag = new Button(t, i);
-
-      tag.setOnAction(
-          e -> {
-            image.removeTag(tag.getText());
-            f.getChildren().remove(tag);
-            updateLog();
-            imageNameUpdate();
-          });
-
-      f.getChildren().add(tag);
-    }
-
-    return f;
-  }
-
-  /** Update the log with its new information. */
-  private void updateLog() {
-
-    Log imageLog = image.getLog();
-    StringBuilder logs = new StringBuilder();
-
-    // add all logs as a line
-    for (Entry e : imageLog) {
-
-      // https://stackoverflow.com/questions/15977295/control-for-displaying-multiline-text
-      logs.append(e.toString()).append(System.lineSeparator());
-    }
-
-    // represent log as textArea
-    log.setText(logs.toString());
-  }
-
-
-  public Scene getImageScene() {
-    return imageScene;
-  }
 }
