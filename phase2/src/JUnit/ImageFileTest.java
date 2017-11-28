0c84b0dc9dccc8e4d97e6a58a651260f5efb5233
Amarnath Parthiban <amarnath.parthiban@mail.utoronto.ca>
2017-11-27 20:33:09 -0500

    Added JUnit class' basic skeleton.

---
+++ phase2/src/JUnit/ImageFileTest.java
@@ -0,0 +1,45 @@
+package JUnit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageFileTest {
+
+    @Test
+    void rename() {
+    }
+
+    @Test
+    void revertName() {
+    }
+
+    @Test
+    void getPriorNames() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void addTag() {
+    }
+
+    @Test
+    void addTag1() {
+    }
+
+    @Test
+    void removeTag() {
+    }
+
+    @Test
+    void removeTag1() {
+    }
+
+    @Test
+    void nameWithTags() {
+    }
+
+}
---
+++ phase2/src/JUnit/ImageManagerTest.java
@@ -0,0 +1,29 @@
+package JUnit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageManagerTest {
+
+    @Test
+    void getImageFilesInSubDirectory() {
+    }
+
+    @Test
+    void getImageFilesInParentDirectory() {
+    }
+
+    @Test
+    void createImagesFromDirectory() {
+    }
+
+    @Test
+    void deleteGlobalTag() {
+    }
+
+    @Test
+    void addGlobalTag() {
+    }
+
+}
