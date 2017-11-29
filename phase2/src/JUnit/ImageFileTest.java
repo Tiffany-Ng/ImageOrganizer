45d96366d0f1f1f943e54419246c0b023af0f1f7
Prynciss Ng <prynciss.ng@mail.utoronto.ca>
2017-11-29 04:10:32 -0500

    Added JUnit tests in ImageFileTest.

--- phase2/src/JUnit/ImageFileTest.java
+++ phase2/src/JUnit/ImageFileTest.java
@@ -1,63 +1,88 @@
 package JUnit;
 
 import ManageImage.ImageFile;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageFileTest {
 
-    private static ImageFile k;
+    private static ImageFile imageFile;
 
     @BeforeAll
     static void setup() {
 
         try {
-            k = new ImageFile(new File("src/JUnit/testImages/1.jpeg"));
-        } catch (Exception e){
-
+            imageFile = new ImageFile(new File("src/JUnit/testImages/1.jpg"));
+        } catch (Exception e) {
             e.printStackTrace();
-
         }
 
 
     }
 
+    @BeforeEach
+    void setupEach() {
+
+        try {
+            imageFile = new ImageFile(new File("src/JUnit/testImages/1.jpg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @AfterEach
+    void afterEach() {
+        imageFile.rename("1");
+        if(imageFile.getTags().size() >0)
+        imageFile.removeTag(imageFile.getTags());
+    }
+
     @Test
     void getNameTest() {
 
-        assertEquals("1", k.getName());
+        assertEquals("1", imageFile.getName());
 
     }
 
     @Test
     void rename() {
 
-        k.rename("newPic");
-        assertEquals("newPic", k.getName());
+        imageFile.rename("newPic");
+        assertEquals("newPic", imageFile.getName());
 
     }
 
     @Test
     void revertName() {
 
-        k.revertName(0);
-        assertEquals("1", k.getName());
+        imageFile.rename("newPic");
+        assertEquals("newPic", imageFile.getName());
+
+        imageFile.revertName(0);
+        assertEquals("1", imageFile.getName());
+
 
     }
 
     @Test
     void getPriorNames() {
 
-        ArrayList<String> namesGotten = k.getPriorNames();
-        ArrayList<String> nameGen = new ArrayList<>();
-        nameGen.add("newPic.jpeg");
-        nameGen.add("1.jpeg");
+        imageFile.rename("newPic");
+        assertEquals("newPic", imageFile.getName());
+
+        ArrayList<String> namesGotten = imageFile.getPriorNames();
+        ArrayList<String> nameGen = new ArrayList<>(Arrays.asList("1.jpg", "newPic.jpg"));
 
         assertEquals(nameGen, namesGotten);
 
@@ -68,15 +93,15 @@ class ImageFileTest {
     void move() {
 
         try {
-            k.move(new File("src/JUnit/testImages/sub"));
+            imageFile.move(new File("src/JUnit/testImages/sub"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        assertEquals(new File("src/JUnit/testImages/sub"), k.getDirectory());
+        assertEquals(new File("src/JUnit/testImages/sub"), imageFile.getDirectory());
 
         try {
-            k.move(new File("src/JUnit/testImages"));
+            imageFile.move(new File("src/JUnit/testImages"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -85,22 +110,57 @@ class ImageFileTest {
 
     @Test
     void addTag() {
+        imageFile.addTag("Tiger");
+        ArrayList tags = imageFile.getTags();
+        ArrayList<String> expectedTags = new ArrayList<>();
+        expectedTags.add("Tiger");
+        assertEquals(expectedTags, tags);
     }
 
     @Test
     void addTag1() {
+        // https://stackoverflow.com/questions/21696784/how-to-declare-an-arraylist-with-values Date: Nov 29 2017
+        ArrayList<String> toAdd = new ArrayList<>(Arrays.asList("Snow", "Cool"));
+
+        imageFile.addTag(toAdd);
+        ArrayList tags = imageFile.getTags();
+        ArrayList<String> expectedTags = new ArrayList<>();
+        expectedTags.addAll(toAdd);
+        assertEquals(expectedTags, tags);
     }
 
     @Test
     void removeTag() {
+        imageFile.addTag("Tiger");
+        imageFile.addTag("Cool");
+        imageFile.removeTag("Tiger");
+
+        ArrayList tags = imageFile.getTags();
+        ArrayList<String> expectedTags = new ArrayList<>();
+        expectedTags.add("Cool");
+        assertEquals(expectedTags, tags);
     }
 
     @Test
     void removeTag1() {
+        imageFile.addTag(new ArrayList<>(Arrays.asList("Snow", "Wild", "Awesome")));
+        imageFile.removeTag(new ArrayList<>(Arrays.asList("Snow", "Awesome")));
+
+        ArrayList tags = imageFile.getTags();
+        ArrayList<String> expectedTags = new ArrayList<>();
+        expectedTags.add("Wild");
+
+        assertEquals(expectedTags, tags);
+
     }
 
     @Test
     void nameWithTags() {
+        imageFile.addTag(new ArrayList<>(Arrays.asList("Snow", "Wild")));
+        String actualName = imageFile.nameWithTags();
+        String expectedName = "1 @Snow @Wild.jpg";
+
+        assertEquals(expectedName, actualName);
     }
 
 }
